16c587b55e7d677a1b8383f3aa7199c29bfe07ee
ahmadreza <djahmadfire@gmail.com>
2023-09-15 22:49:14 +0330

    Added advance options

--- resize-core/src/lib.rs
+++ resize-core/src/lib.rs
@@ -17,6 +17,37 @@ pub extern  "C" fn resize(buff:Vec<u8>, w:u32, h:u32)->Vec<u8>{
 fn resizer_core(imgbuff:Vec<u8>, w:u32, h:u32)->std::io::Cursor<Vec<u8>>{
     let img = image::load_from_memory(&imgbuff).unwrap();
     let result = img.resize(w, h, image::imageops::Lanczos3);
+    
+    let mut wt = std::io::Cursor::new(Vec::new());
+    result.write_to(&mut wt, image::ImageFormat::Png).unwrap();
+    wt
+}
+
+#[wasm_bindgen]
+pub extern  "C" fn resize_advance(buff:Vec<u8>, w:u32, h:u32, resize_mode: &str, filter:&str)->Vec<u8>{
+    let resized_buff = advance_resizer_core(buff, w, h, resize_mode, filter);
+    resized_buff.get_ref().to_owned()
+}
+
+fn filter_pick(filter:&str)->image::imageops::FilterType{
+    match filter {
+        "CatmullRom" => image::imageops::CatmullRom,
+        "Gaussian" => image::imageops::Gaussian,
+        "Lanczos3" => image::imageops::Lanczos3,
+        "Nearest" => image::imageops::Nearest,
+        "Triangle" => image::imageops::Triangle,
+        _=> image::imageops::Lanczos3
+    }
+}
+// NEW
+fn advance_resizer_core(imgbuff:Vec<u8>, w:u32, h:u32, resize_mode: &str, filter:&str)->std::io::Cursor<Vec<u8>>{
+    let img = image::load_from_memory(&imgbuff).unwrap();
+    let result = match resize_mode {
+        "resize"=> img.resize(w, h, filter_pick(filter)),
+        "resize_exact"=> img.resize_exact(w, h, filter_pick(filter)),
+        "resize_to_fill"=> img.resize_to_fill(w, h, filter_pick(filter)),
+        _=> img.resize(w, h, filter_pick(filter)),
+    };
 
     let mut wt = std::io::Cursor::new(Vec::new());
     result.write_to(&mut wt, image::ImageFormat::Png).unwrap();
--- resizer-js/src/main/Main.jsx
+++ resizer-js/src/main/Main.jsx
@@ -1,5 +1,5 @@
 import { useRef, useState } from "react";
-import { resize } from "../core/resizer_core";
+import { resize_advance } from "../core/resizer_core";
 
 function Main() {
     const [imageLink, setImageLink] = useState({ "blob": new Blob(), "link": "" });
@@ -10,6 +10,8 @@ function Main() {
     // I'm too lazy to setup a controller...
     let Width = useRef();
     let Height = useRef();
+    let Resize_mode = useRef();
+    let Filter = useRef();
 
     async function getBuffer() {
         // get image buffer
@@ -21,8 +23,17 @@ function Main() {
         });
     }
 
-    async function resizer_fn() {
-        let newbuff = resize(new Uint8Array(await imageLink.blob.arrayBuffer()), Width.current.value || 500, Height.current.value || 500);
+    // async function resizer_fn() {
+    //     let newbuff = resize(new Uint8Array(await imageLink.blob.arrayBuffer()), Width.current.value || 500, Height.current.value || 500);
+    //     setResizedimageLink(URL.createObjectURL(new Blob([newbuff], { type: "image/png" })));
+    // }
+    async function resizer_advance_fn() {
+        // resize modes => resize , resize_exact , resize_to_fill
+        // filter types => CatmullRom , Gaussian , Lanczos3 , Nearest , Triangle
+        let newbuff = resize_advance(
+            new Uint8Array(await imageLink.blob.arrayBuffer()), Width.current.value || 500, Height.current.value || 500,
+            Resize_mode.current.value , Filter.current.value
+        );
         setResizedimageLink(URL.createObjectURL(new Blob([newbuff], { type: "image/png" })));
     }
     return (
@@ -33,13 +44,31 @@ function Main() {
             </div>
             <div className="fileoutput">
                 <div>
-                    <label htmlFor="Width">Width: </label>
-                    <input type="number" id="Width" ref={Width}/>
-                    <br />
-                    <label htmlFor="Height">Height: </label>
-                    <input type="number" id="Height" ref={Height}/>
-                    <br />
-                    <button onClick={resizer_fn}>resize</button>
+                    <div>
+                        <div>
+                            <label htmlFor="Width">Width</label>
+                            <label htmlFor="Height">Height</label>
+                            <label htmlFor="Resize_type">Resize type</label>
+                            <label htmlFor="Filters">Filters</label>
+                        </div>
+                        <div>
+                            <input type="number" id="Width" ref={Width}/>
+                            <input type="number" id="Height" ref={Height}/>
+                            <select id="Resize_type" ref={Resize_mode}>
+                                <option value="resize">resize</option>
+                                <option value="resize_exact">resize with stretch</option>
+                                <option value="resize_to_fill">resize with crop</option>
+                            </select>
+                            <select id="Filters" ref={Filter}>
+                                <option value="Lanczos3">Lanczos3</option>
+                                <option value="CatmullRom">CatmullRom</option>
+                                <option value="Gaussian">Gaussian</option>
+                                <option value="Nearest">Nearest</option>
+                                <option value="Triangle">Triangle</option>
+                            </select>
+                        </div>
+                    </div>
+                    <button onClick={resizer_advance_fn}>resize</button>
                 </div>
                 <a target="_blank" rel="noreferrer" href={resizedimageLink}><img src={resizedimageLink} /></a>
             </div>
